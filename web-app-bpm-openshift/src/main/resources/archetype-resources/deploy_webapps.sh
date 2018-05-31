ef39e6a5c203a2d689d02f512d056b971a8cc5e7
Ampie Barnard <ampie.private@gmail.com>
2018-05-31 11:40:44 +0200

    EN-1330 migrated to 5.0.0 Final for the Openshift archetype

--- web-app-bpm-openshift/src/main/resources/archetype-resources/deploy_webapps.sh
+++ web-app-bpm-openshift/src/main/resources/archetype-resources/deploy_webapps.sh
@@ -1,5 +1,5 @@
 ENTANDO_SERVICE_URL=$(oc describe route ${artifactId}-service|grep -oP "(?<=Requested\sHost:\t\t)[^ ]+")
-ENTANDO_VERSION=5.0.0-SNAPSHOT
+ENTANDO_VERSION=5.0.0
 oc new-app --name ${artifactId}-mapp-engine-admin-app --docker-image entando/mapp-engine-admin-app-openshift:$ENTANDO_VERSION -e USE_MOCKS=false -e DOMAIN=$ENTANDO_SERVICE_URL
 oc expose svc ${artifactId}-mapp-engine-admin-app
 oc new-app --name ${artifactId}-app-builder --docker-image entando/app-builder-openshift:$ENTANDO_VERSION -e USE_MOCKS=false -e DOMAIN=$ENTANDO_SERVICE_URL
--- web-app-bpm-openshift/src/main/resources/archetype-resources/pull_images.sh
+++ web-app-bpm-openshift/src/main/resources/archetype-resources/pull_images.sh
@@ -2,6 +2,6 @@ eval $(minishift docker-env)
 IMAGES=( "entando-fabric8s2i-wildfly-12" "entando-fabric8s2i-postgresql-95" "mapp-engine-admin-app-openshift" "app-builder-openshift" )
 #[[for IMAGE in "${IMAGES[@]}"]]#
 do
-  echo "Pulling image entando/$IMAGE:5.0.0-SNAPSHOT"
-  docker pull entando/$IMAGE:5.0.0-SNAPSHOT
+  echo "Pulling image entando/$IMAGE:5.0.0"
+  docker pull entando/$IMAGE:5.0.0
 done
